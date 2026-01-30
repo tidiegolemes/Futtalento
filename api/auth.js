fc662e2a8aeb9a56e54dd27cfbf77fdbacefaa07
Diego Lemes <137827241+tidiegolemes@users.noreply.github.com>
2026-01-30 16:17:16 -0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,12 @@
 module.exports = async (req, res) => {
   const { scope, state } = req.query;
+  
+  // GARANTA QUE O NOME ABAIXO SEJA IGUAL AO DA VERCEL (TUDO MAIÚSCULO)
   const client_id = process.env.GITHUB_CLIENT_ID;
   
-  // Redireciona para o GitHub para iniciar a autorização
+  if (!client_id) {
+    return res.status(500).send("Erro: GITHUB_CLIENT_ID não encontrado nas variáveis da Vercel.");
+  }
+
   res.redirect(`https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user&state=${state}`);
 };

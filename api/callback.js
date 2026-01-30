6d50125cf45fdd4fbc1b896f35f745c1e60dd652
Diego Lemes <137827241+tidiegolemes@users.noreply.github.com>
2026-01-30 16:09:38 -0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,1 +1,27 @@
+const axios = require('axios');
 
+module.exports = async (req, res) => {
+  const { code } = req.query;
+
+  try {
+    const response = await axios.post('https://github.com/login/oauth/access_token', {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    }, {
+      headers: { Accept: 'application/json' }
+    });
+
+    const token = response.data.access_token;
+    
+    // Este script devolve o token para a janela do seu Admin
+    res.send(`
+      <script>
+        const content = { token: "${token}", provider: "github" };
+        window.opener.postMessage('authorization:github:success:' + JSON.stringify(content), window.location.origin);
+      </script>
+    `);
+  } catch (error) {
+    res.status(500).send("Erro na autenticação: " + error.message);
+  }
+};

c24efe46fb32002af11bb5365c92fadb3c7dc6c9
Diego Lemes <137827241+tidiegolemes@users.noreply.github.com>
2026-01-30 16:09:26 -0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,1 +1,7 @@
-api/auth.js
+module.exports = async (req, res) => {
+  const { scope, state } = req.query;
+  const client_id = process.env.GITHUB_CLIENT_ID;
+  
+  // Redireciona para o GitHub para iniciar a autorização
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user&state=${state}`);
+};
